dcd9186e2c71901c8a89231ac0f1e275ddf40766
lamvantieu <lyhoanglam3@gmail.com>
2025-12-26 12:22:39 +0700

    Update page.tsx

--- app/courses/edit/[id]/page.tsx
+++ app/courses/edit/[id]/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { useRouter, useParams } from "next/navigation";
 import { Form, Input, Button, message } from "antd";
 import api from "@/lib/axios";
-import type { Course } from "@/types/course";
+import type { Course } from "@/type/course";
 
 const EditCoursePage = () => {
     const router = useRouter();
@@ -55,3 +55,4 @@ const EditCoursePage = () => {
 };
 
 export default EditCoursePage;
+
